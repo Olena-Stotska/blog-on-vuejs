e065d88274a9a0ccb265b7e28b42ec3eecb64497
Lena Stotska <i.olena.stotska@gmail.com>
2018-07-09 21:23:14 +0300

    feat(post): add part style for new post component

--- src/components/NewPosts.vue
+++ src/components/NewPosts.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
+  <div class="create-post">
+    <div class="header-post">
+      <button :title="message" class="btn-home">
+        <router-link to="/"><img src="../assets/img/brain.svg" alt="Logo"></router-link>
+      </button>
+      <h1>New Post</h1>
+    </div>
+
     <form>
       <label class="steps-post">
-        <div>Title<span class="form-asterisk">*</span></div>
+        <div class="step-name">Title<span class="form-asterisk">*</span></div>
         <transition name="fadeRight">
           <div class="validation" v-if="!isValidTitle">Write your publication name!</div>
         </transition>
@@ -10,7 +17,7 @@
       </label>
 
       <label class="steps-post">
-        <div>Description<span class="form-asterisk">*</span></div>
+        <div class="step-name">Description<span class="form-asterisk">*</span></div>
         <transition name="fadeRight">
           <div class="validation" v-if="!isValidDescription">Write a short Description!</div>
         </transition>
@@ -20,10 +27,14 @@
 
     <vue-editor v-model="content"></vue-editor>
 
-    <input class="tag" type="text" v-model.trim="tags" placeholder="Add tags">
+    <div class="tags">
+      <label class="step-name">Tags</label>
+      <input type="text" v-model.trim="tags" placeholder="Add tags...">
+      <div class="tag-output"></div>
+    </div>
 
-    <div class="theme">
-       <select v-model="selected">
+    <div class="select">
+       <select v-model="selected" id="slct">
         <option disabled value="">Select a theme...</option>
         <option>Tech</option>
         <option>Politics</option>
@@ -51,6 +62,7 @@ export default {
     VueEditor
   },
   data: () => ({
+    message: 'To home',
     title: '',
     description: '',
     content: '',
@@ -81,3 +93,153 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+@import '@/styles/variables.scss';
+
+.create-post {
+  padding: 15px 10px;
+
+}
+
+.header-post {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    flex: 1;
+    margin-left: 15px;
+  }
+}
+
+.btn-home {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  img {
+    width: 60px;
+    height: 60px;
+  }
+}
+
+form {
+  margin: 20px 0;
+
+  input {
+    font-size: 1rem;
+    font-weight: 500;
+    border: transparent;
+    border-bottom: 1px solid map-get($colors, border);
+    outline: none;
+
+    &::placeholder {
+      color: map-get($colors, primary);
+      font-weight: 700;
+    }
+  }
+
+  .form-asterisk {
+    font-size: 1.8rem;
+  }
+}
+
+.steps-post {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.step-name {
+  font-size: 1.2rem;
+  font-weight: 700;
+}
+
+.tags {
+  margin: 30px 0;
+
+  .step-name {
+    margin-right: 10px;
+  }
+
+  input {
+    border: none;
+    font-size: 1.2rem;
+    padding: 10px;
+    border-radius: 10px;
+    width: 100px;
+    outline: none;
+    background-color: map-get($colors, border);
+  }
+}
+
+// .theme {
+//   margin: 30px 0;
+
+  // select {
+  //   background-color: map-get($colors, border);
+  //   border: none;
+  //   padding: 10px;
+  //   border-radius: 10px;
+  //   font-size: 1.2rem;
+  //   outline: none;
+  //   margin-right: 10px;
+  // }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: #2c3e50;
+    background-image: none;
+  }
+  /* Custom Select */
+  .select {
+    position: relative;
+    display: block;
+    width: 20em;
+    height: 3em;
+    line-height: 3;
+    background: #2c3e50;
+    overflow: hidden;
+    border-radius: .25em;
+  }
+  select {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 .5em;
+    color: #fff;
+    cursor: pointer;
+  }
+  select::-ms-expand {
+    display: none;
+  }
+  /* Arrow */
+  .select::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 1em;
+    background: #34495e;
+    pointer-events: none;
+  }
+  /* Transition */
+  .select:hover::after {
+    color: #f39c12;
+  }
+  .select::after {
+    -webkit-transition: .25s all ease;
+    -o-transition: .25s all ease;
+    transition: .25s all ease;
+  }
+// }
+</style>
