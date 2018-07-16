d93225eff1730e6f9d113a15ba54e5b6fdfb364a
Lena Stotska <i.olena.stotska@gmail.com>
2018-07-16 15:31:24 +0300

    feat(listPost): add message if drafts or published list empty

--- src/components/ListDrafts.vue
+++ src/components/ListDrafts.vue
@@ -1,5 +1,8 @@
 <template>
-  <ListArticles :articles="getDraftsByUserId" />
+  <div>
+    <ListArticles :articles="getDraftsByUserId" />
+    <div v-show="!noArticles" class="empty-list">You haven't any drafts yet.</div>
+  </div>
 </template>
 
 <script>
@@ -9,11 +12,29 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'ListDrafts',
+  data: () => ({
+    emptyDrafts: false
+  }),
   components: {
     ListArticles
   },
   computed: {
     ...mapGetters(['getDraftsByUserId']),
+
+    emptyArticles() {
+      if (this.getDraftsByUserId.length === 0) {
+        this.emptyDrafts = true
+      }
+    }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.empty-list {
+  text-align: center;
+  margin-top: 10%;
+  font-weight: 700;
+  font-size: 1.5rem;
+}
+</style>
--- src/components/ListPublic.vue
+++ src/components/ListPublic.vue
@@ -1,5 +1,8 @@
 <template>
-  <ListArticles :articles="getPostsByUserId" />
+  <div>
+    <ListArticles :articles="getPostsByUserId" />
+    <div v-show="!noArticles" class="empty-list">You haven’t published any public stories yet.</div>
+  </div>
 </template>
 
 <script>
@@ -9,15 +12,29 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'ListPublic',
+  data: () => ({
+    emptyPublic: false,
+  }),
   components: {
     ListArticles
   },
   computed: {
-    ...mapGetters(['getPostsByUserId'])
+    ...mapGetters(['getPostsByUserId']),
+
+    emptyArticles() {
+      if (this.getPostsByUserId.length === 0) {
+        this.emptyPublic = true
+      }
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/post-style.scss';
+.empty-list {
+  text-align: center;
+  margin-top: 10%;
+  font-weight: 700;
+  font-size: 1.5rem;
+}
 </style>
