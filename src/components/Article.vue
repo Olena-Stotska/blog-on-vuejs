9815c7877ef3995051729494febcbe8806ad34a6
Lena Stotska <i.olena.stotska@gmail.com>
2018-07-18 15:52:54 +0300

    feat(article): component Article for to read post

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <div class="container">
-      <h1>{{ post.title }}</h1>
-
+      <h1 class="title">{{ post.title }}</h1>
+      <p class="description">{{ post.description }}</p>
+      <img :src="post.image" alt="Post Image">
+      <p class="content" v-html="post.content"></p>
+      <div class="tags">
+        <div class="tag" v-for="tag in post.tags" :key="tag">
+          {{ tag }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,5 +41,53 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/variables.scss';
 
+.container {
+  text-align: left;
+  padding: 20px;
+
+  .description {
+    font-style: italic;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  .content,
+  .description {
+    font-size: 1.2rem;
+    line-height: 2rem;
+
+      h4 {
+      font-size: 1.8rem;
+    }
+  }
+}
+
+.tags {
+  display: flex;
+
+  .tag {
+    padding: 5px 10px;
+    margin: 10px 10px 0 0;
+    font-size: 1.4rem;
+    border-radius: 4px;
+    font-weight: 700;
+    display: inline-block;
+    color: map-get($colors, primary);
+    background-color: map-get($colors, border);
+  }
+}
+
+@media(min-width: #{map-get($breakpoints, small)}) {
+  .container {
+    margin: 0 5%;
+
+    .content {
+      font-size: 1.3rem;
+    }
+  }
+}
 </style>
